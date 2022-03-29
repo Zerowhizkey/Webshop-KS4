d09570b3ca169b53ba1b4f5ec6622aacfde9ff5f
Zerowhizkey <marcus.piri@cmeducations.se>
2022-03-29 15:05:04 +0200

    fixed some axios things and got login to work

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Layout from "../components/Layout";
 import Box from "@mui/material/Box";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import authState from "../stores/auth/atom";
 import { Button } from "@mui/material";
-
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 function Login() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const users = useRecoilValue(authState);
+	const [auth, setAuth] = useRecoilState(authState);
 	console.log(users);
 
 	const [data, setData] = useState({
@@ -19,17 +21,30 @@ function Login() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const userData = {
-			email: data.email,
-			password: data.password,
-		};
+		// authState = {
+		// 	username: data.username,
+		// 	password: data.password,
+		// };
+		// eyJr389hbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+		// eyJr389hbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
 		axios
-			.post("https://k4backend.osuka.dev/auth/login", userData)
+			.post(
+				"https://k4backend.osuka.dev/auth/login",
+				// { username: "mor_2314", password: "83r5^_" },
+				{ username: username, password: password },
+				users
+			)
 			.then((response) => {
 				console.log(response.status);
 				console.log(response.data.token);
+				setAuth({
+					...auth,
+					token: response.data.token,
+				});
 			});
 	};
+
+	if (auth.token) return <Navigate to="/profile" />;
 
 	return (
 		<Layout>
@@ -55,7 +70,6 @@ function Login() {
 						id="outlined-password-input"
 						label="Password"
 						type="password"
-						autoComplete="current-password"
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
