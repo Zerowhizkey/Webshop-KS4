aeab5ba7d5c39a2c4bf92daf65c3cbcd56514a43
Zerowhizkey <marcus.piri@cmeducations.se>
2022-03-30 16:02:45 +0200

    final not final

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Layout from "../components/Layout";
 import { Button, Typography, TextField, Grid } from "@mui/material";
 import authState from "../stores/auth/atom";
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 // import { Navigate } from "react-router-dom";
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 // import { useRecoilValue, useRecoilState } from "recoil";
 
 function Register() {
-	const [auth, setAuth] = useRecoilState(authState);
+	const setAuth = useSetRecoilState(authState);
 	const [user, setUser] = useState({
 		email: "",
 		username: "",
